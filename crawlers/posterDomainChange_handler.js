7e6a8456e0a12828dceca170ee8bcd735925076c
ashkan-esz <58337265+ashkan-esz@users.noreply.github.com>
2020-10-19 12:31:48 +0330

    feature : source domain change handler

---
+++ crawlers/posterDomainChange_handler.js
@@ -0,0 +1,153 @@
+const axios = require('axios').default;
+import axiosRetry from "axios-retry";
+axiosRetry(axios, {
+    retries: 4, retryDelay: (retryCount) => {
+        return retryCount * 1000;
+    }
+});
+import getCollection from "../mongoDB";
+import {save_error} from "../save_logs";
+
+module.exports = async function(sourcesObject){
+    try {
+        let sources = [];
+        sources.push(sourcesObject.digimovies.movie_url);
+        sources.push(sourcesObject.film2media.movie_url);
+        sources.push(sourcesObject.film2movie.movie_url);
+        sources.push(sourcesObject.mrmovie.movie_url);
+        sources.push(sourcesObject.salamdl.movie_url);
+        sources.push(sourcesObject.topmovies.movie_url);
+        sources.push(sourcesObject.valamovie.movie_url);
+        let dominNames = sources.map(value => value.replace(/www.|https:\/\/|\/page\/|\//g, ''));
+
+        let isChanged = false;
+        for (let i = 0; i < dominNames.length; i++) {
+            let response = await axios.get('https://www.' + dominNames[i]);
+            let responseUrl = response.request.res.responseUrl;
+            responseUrl = purify(responseUrl);
+            if (dominNames[i] !== responseUrl) {
+                //changed
+                sources[i] = sources[i].replace(dominNames[i].split('.')[1], responseUrl.split('.')[1]);
+                dominNames[i] = responseUrl;
+                isChanged = true;
+            }
+        }
+
+        if (isChanged) {
+            //handle poster link update
+            await updatePosterLinks(sources, dominNames, 'movies');
+            await updatePosterLinks(sources, dominNames, 'serials');
+            //update fields
+            updateSourceFields(sourcesObject, sources, dominNames);
+            // update sources in db
+            let collection = await getCollection('sources');
+            await collection.findOneAndReplace({title: 'sources'}, sourcesObject);
+            save_error({title: 'source domain changed', date: new Date()});
+        }
+    }catch (error) {
+        error.massage = "module: search_tools >> wrapper_module ";
+        error.time = new Date();
+        save_error(error);
+    }
+};
+
+async function updatePosterLinks(sources, dominNames, collectionName) {
+    //handle movies
+    let collection = await getCollection(collectionName);
+    let docs_array = await collection.find({}, {projection: {poster: 1}}).toArray();
+
+    for (let i = 0; i < docs_array.length; i++) {
+        let changed = false;
+        let posters = docs_array[i].poster;
+        for (let j = 0; j < posters.length; j++) {
+            if (posters[j].includes('digimovies')) {
+                let temp = domainChangeHandler(posters[j], dominNames[0]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            } else if (posters[j].includes('film2media')) {
+                let temp = domainChangeHandler(posters[j], dominNames[1]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            } else if (posters[j].includes('film2movie')) {
+                let temp = domainChangeHandler(posters[j], dominNames[2]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            } else if (posters[j].includes('mrmovie')) {
+                let temp = domainChangeHandler(posters[j], dominNames[3]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            } else if (posters[j].includes('salamdl')) {
+                let temp = domainChangeHandler(posters[j], dominNames[4]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            } else if (posters[j].includes('topmovies')) {
+                let temp = domainChangeHandler(posters[j], dominNames[5]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            } else if (posters[j].includes('valamovie')) {
+                let temp = domainChangeHandler(posters[j], dominNames[6]);
+                if (temp!==posters[j]){
+                    posters[j] = temp;
+                    changed = true;
+                }
+            }
+        }
+
+        if (changed) {
+            //update posters
+            await collection.findOneAndUpdate({_id: docs_array[i]._id}, {
+                $set: {
+                    posters: posters
+                }
+            })
+        }
+    }
+}
+
+function updateSourceFields(sourcesObject, sources, dominNames) {
+    sourcesObject.digimovies.movie_url = sources[0];
+    let temp = sourcesObject.digimovies.serial_url;
+    sourcesObject.digimovies.serial_url = temp.replace(temp.split('.')[1], dominNames[0].split('.')[1]);
+
+    sourcesObject.film2media.movie_url = sources[1];
+    sourcesObject.film2movie.movie_url = sources[2];
+
+    sourcesObject.mrmovie.movie_url = sources[3];
+    temp = sourcesObject.mrmovie.serial_url;
+    sourcesObject.mrmovie.serial_url = temp.replace(temp.split('.')[1], dominNames[3].split('.')[1]);
+
+    sourcesObject.salamdl.movie_url = sources[4];
+
+    sourcesObject.topmovies.movie_url = sources[5];
+    temp = sourcesObject.topmovies.serial_url;
+    sourcesObject.topmovies.serial_url = temp.replace(temp.split('.')[1], dominNames[5].split('.')[1]);
+
+    sourcesObject.valamovie.movie_url = sources[6];
+    temp = sourcesObject.topmovies.serial_url;
+    sourcesObject.valamovie.serial_url = temp.replace(temp.split('.')[1], dominNames[6].split('.')[1]);
+}
+
+function domainChangeHandler(poster, dominName) {
+    let prevDomain = purify(poster).split('.')[1];
+    let newDomain = dominName.split('.')[1];
+    if (prevDomain !== newDomain) {
+        return poster.replace(prevDomain, newDomain);
+    }
+    return poster;
+}
+
+function purify(input) {
+    return input.replace(/www.|https:\/\/|\/page\/|\//g,'').split('.').filter(value =>value && !value.includes('image')).join('.');
+}
--- crawlers/start_crawling.js
+++ crawlers/start_crawling.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const film2media = require('./sources/film2media');
 const digimovies = require('./sources/digimovies');
 const salamdl = require('./sources/salamdl');
@@ -6,6 +5,8 @@ const film2movie = require('./sources/film2movie');
 const mrmovie = require('./sources/mrmovie');
 const topmovies = require('./sources/topmovies');
 const valamovie = require('./sources/valamovie');
+import getCollection from "../mongoDB";
+const posterDomainChange_handler = require('./posterDomainChange_handler');
 import {save_error} from "../save_logs";
 
 async function start_crawling() {
@@ -13,16 +14,18 @@ async function start_crawling() {
         try {
             let time1 = new Date();
 
-            let json_file = fs.readFileSync('./crawlers/sources.json', 'utf8')
-            let saved_array = JSON.parse(json_file);
+            let collection = await getCollection('sources');
+            let sources = await collection.findOne({title:'sources'});
 
-            await digimovies({...saved_array.digimovies});
-            await film2media({...saved_array.film2media});
-            await film2movie({...saved_array.film2movie});
-            await mrmovie({...saved_array.mrmovie});
-            await salamdl({...saved_array.salamdl});
-            await topmovies({...saved_array.topmovies});
-            await valamovie({...saved_array.valamovie});
+            await digimovies(sources.digimovies);
+            await film2media(sources.film2media);
+            await film2movie(sources.film2movie);
+            await mrmovie(sources.mrmovie);
+            await salamdl(sources.salamdl);
+            await topmovies(sources.topmovies);
+            await valamovie(sources.valamovie);
+
+            await posterDomainChange_handler(sources);
 
             let time2 = new Date();
             let crawling_time = time2.getTime() - time1.getTime();
