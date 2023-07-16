4051dbdb8bc31ad21dc90b05199669df8648fa0e
ashkan <ashkanaz2828@gmail.com>
2023-07-16 11:11:09 +0330

    feat(docker): add adminPanel to docker-compose

--- readme/API.USER.README.md
+++ readme/API.USER.README.md
@@ -56,6 +56,7 @@
         'http://127.0.0.1:3000',
         'http://localhost:3000',
         'http://127.0.0.1:5000',
+        'http://localhost:8080',
         'http://localhost:5000'
     ]**
 
--- src/config/configsDb.js
+++ src/config/configsDb.js
@@ -62,6 +62,7 @@ export const defaultConfigsDb = Object.freeze({
         'http://localhost:3000',
         'http://127.0.0.1:5000',
         'http://localhost:5000',
+        'http://localhost:8080',
     ]),
     disableTestUserRequests: false,
     disableCrawlerForDuration: 0,
