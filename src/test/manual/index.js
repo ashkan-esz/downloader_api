4ab39ae89859f7137256a7cf74b943d495e7e1e4
ashkan <ashkanaz2828@gmail.com>
2023-08-17 18:04:06 +0330

    Chore: remove manual service test module

--- src/test/manual/index.js
+++ src/test/manual/index.js
@@ -1,6 +1,4 @@
-import * as likeService from './mtest_userStatsService.js';
 
 export async function startManualTest() {
-    await likeService.testLikeService();
-    await likeService.testLikeTransaction();
+
 }
