92a6b7f57686e33b2a7494b3aabf64bc67a5b009
ashkan <ashkanaz2828@gmail.com>
2024-09-23 20:00:35 +0330

    feat: add role model

---
+++ src/data/db/admin/roleAndPermissionsDbMethods.js
@@ -0,0 +1,13 @@
+
+export const PermissionsList = Object.freeze({
+    torrent_search: "torrent_search",
+    torrent_leach: "torrent_leach",
+    crawl_source: "crawl_source",
+    //-----------------------------
+    admin_create_role: "admin_create_role",
+    admin_edit_role: "admin_edit_role",
+    admin_delete_role: "admin_delete_role",
+    //-----------------------------
+    admin_create_admin: "admin_create_admin",
+    admin_remove_admin: "admin_remove_admin",
+});
